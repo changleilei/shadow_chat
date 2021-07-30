d07109cc41e68b8ec0ea5246a83a13216a3753df
changleilei <852324033@qq.com>
2021-07-30 18:27:13 +0800

    shared nlu  and exceptions

---
+++ sasa/shared/exceptions.py
@@ -0,0 +1,12 @@
+# coding=utf8
+"""================================
+@Author: Mr.Chang
+@Date  : 2021/7/30 3:10 下午
+==================================="""
+
+class SasaException(Exception):
+    pass
+
+
+class SasaCoreException(SasaException):
+    """Sasa Core 的基础异常"""
---
+++ sasa/shared/nlu/constants.py
@@ -0,0 +1,8 @@
+# coding=utf8
+"""================================
+@Author: Mr.Chang
+@Date  : 2021/7/30 5:35 下午
+==================================="""
+
+INTENT = "intent"
+INTENT_NAME_KEY = 'name'
